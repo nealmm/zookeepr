36deb64a369d2da5367f8bde9684133e24f361f7
Matt Neal <neal.matt.m@gmail.com>
2022-08-02 22:51:39 -0400

    fixed application error

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { animals } = require('./data/animals.json')
 
+const PORT = process.env.PORT || 3001
+
 const app = express()
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`)
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
 })
 
 function filterByQuery(query, animalsArray) {
